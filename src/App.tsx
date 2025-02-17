2f44060605d6005cc26b82f5adea9af138be645f
Michael Ani <160235779+mikemichael8@users.noreply.github.com>
2025-02-17 22:24:49 +0100

    Add files via upload
    more improvements to be made.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from './components/Header';
+import MainBody from './components/MainBody';
+import Packages from './components/Packages';
+import Footing from './components/Footing';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainBody />
+      <Packages />
+      <Footing />
+    </div>
+    
+  );
+}
+
+
+export default App;
---
+++ src/components/Packages.tsx
@@ -0,0 +1,58 @@
+
+
+function Packages() {
+    return (
+    <>
+        <div className="general-package">
+            <div className="package-category">
+                <h1>FRIENDLY PLANS THAT SUITS YOU</h1>
+            </div>
+            <div className="plan-category">
+                <div className="basic">
+                    <img src="src/images/drink water.jpg" alt="photo1" />
+                    <h3>Basic Plan</h3>
+                    <p>This targets individuals</p>
+                    <h3>N5,000</h3>
+                </div>
+
+                <div className="family">
+                    <img src="src/images/family gym photo.jpg" alt="photo2" />
+                    <h3>Family Plan</h3>
+                    <p>Aimed at families</p>
+                    <h3>N10,000</h3>
+                </div>
+
+                <div className="group">
+                    <img src="src/images/group training photo.jpg" alt="photo3" />
+                    <h3>Group Training</h3>
+                    <p>Group motivations and trainings in groups</p>
+                    <h3>N12,000</h3>
+                </div>
+
+                <div className="pro-athlete">
+                    <img src="src/images/heavy weight.jpg" alt="photo4" />
+                    <h3>Pro Athlete</h3>
+                    <p>Aimed at people with training experiences</p>
+                    <h3>N15,000</h3>
+                </div>
+
+                <div className="special">
+                    <img src="src/images/wheelchair-exercises.webp" alt="photo5" />
+                    <h3>Special Need Plan</h3>
+                    <p>Aimed at people who are physically challenged</p>
+                    <h3>N10,000</h3>
+                </div>
+                <div className="aged">
+                    <img src="src/images/aged-fitness.jpg" alt="photo5" />
+                    <h3>Senior Citizens Plan</h3>
+                    <p>Aimed at people who are elderly</p>
+                    <h3>N5,000</h3>
+                </div>
+            </div>
+        </div>
+    </>
+    );
+}
+
+
+export default Packages;
